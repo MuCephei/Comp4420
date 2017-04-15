053b4680496fa7f09571f18607131e97f33fea3b
MuCephei <garrettsuss@gmail.com>
2017-04-15 13:19:43 -0500

    Commit so i can get my work on my laptop

--- CCTP.py
+++ CCTP.py
@@ -4,69 +4,81 @@ import copy
 from sets import Set
 
 def cyclic_routing(graph):
-	tour = christofides_alg(graph)
-	edges = graph.all_edges
-	vertices = [None, copy.deepcopy(graph.vertices)]
-	path = []
-	s = 0
-	m = 1
-	vertices[m].remove(graph.vertices[s])
-	p = [0]
-	direction = True
-	while len(vertices[m]) > 1:
-		shortcut_path = []
-		for i in range(len(vertices[m])):
-			shortcut_path.append(vertices[m][i])
-		p.append(shortcut_path)
-		print("M " + str(m))
-		print(vertices[m])
-		print(path)
-		if m == 1 or vertices[m][0] == vertices[m-1][len(vertices[m-1]) - 1]:
-			path = shortcut(direction, m, vertices, edges, path, graph.vertices)
-			if vertices[m+1] == vertices[m]:
-				#if nothing happened
-				path = shortcut(not direction, m, vertices, edges, path, graph.vertices)
-		else:
-			direction = not direction
-			path = shortcut(direction, m, vertices, edges, path, graph.vertices)
-		m += 1
-	print(p)
-	return path
+    edges = graph.all_edges
+    vertices = [graph.vertices,[]]
+    tour = []
+    for point in christofides_alg(graph):
+        vertices[1].append(graph.vertices[point.b])
+    path = []
+    s = 0
+    m = 1
+    vertices[m].remove(vertices[0][s])
+    p = [0]
+    direction = True
+    while len(vertices[m]) > 1:
+        shortcut_path = []
+        for i in range(len(vertices[m])):
+            shortcut_path.append(vertices[m][i])
+        p.append(shortcut_path)
+        if m == 1 or vertices[m][0] == vertices[m-1][len(vertices[m-1]) - 1]:
+            path = shortcut(direction, m, vertices, edges, path, vertices[0])
+            if vertices[m+1] == vertices[m]:
+                #if nothing happened
+                path = shortcut(not direction, m, vertices, edges, path, vertices[0])
+        else:
+            direction = not direction
+            path = shortcut(direction, m, vertices, edges, path, vertices[0])
+        m += 1
+    return path
 
 def shortcut(direction, m, vertices, edges, path, all_vertices):
-	#foreward direction is true, backwards is false
-	if not direction:
-		vertices[m] = vertices[m][::-1]
-	i = 0
-	j = 1
-	e = Set()
-	vertices.append(copy.deepcopy(vertices[m]))
-	while j < len(vertices[m]):
-		if edges[_get_index(vertices[m][j], all_vertices)][_get_index(vertices[m][i], all_vertices)]:
-			vertices[m+1].remove(vertices[m][j])
-			path = path + [(_get_index(vertices[m][i], all_vertices), _get_index(vertices[m][j], all_vertices))]
-			i = j
-			j = i + 1
-		else:
-			e.add(((_get_index(vertices[m][j], all_vertices), _get_index(vertices[m][i], all_vertices))))
-			l = _get_index(vertices[m][i], all_vertices) + 1
-			while all_vertices[l] != vertices[m][j] \
-				and (not edges[l][_get_index(vertices[m][i], all_vertices)]\
-					or not edges[_get_index(vertices[m][j], all_vertices)][l]):
-				if not edges[l][_get_index(vertices[m][i], all_vertices)]:
-					e.add(l, _get_index(vertices[m][i], all_vertices))
-				if not edges[_get_index(vertices[m][j], all_vertices)][l]:
-					e.add(_get_index(vertices[m][j], all_vertices), l)
-				l = l + 1
-			if all_vertices[l] == vertices[m][j]:
-				vertices[m+1].remove(vertices[m][j])
-				print(l)
-				path = path + [(_get_index(vertices[m][i], all_vertices), l), (l, _get_index(vertices[m][j], all_vertices))]
-				i = j
-				j = i + 1
-			else:
-				j = j + 1
-	return path
+    #foreward direction is true, backwards is false
+    if not direction:
+        vertices[m] = vertices[m][::-1]
+    i = 0
+    j = 1
+    e = Set()
+    vertices.append(copy.deepcopy(vertices[m]))
+    while j < len(vertices[m]):
+        mj = _get_index(vertices[m][j], all_vertices)
+        mi = _get_index(vertices[m][i], all_vertices)
+        ij_min = min(mi, mj)
+        ij_max = max(mi,mj)
+        if edges[ij_max][ij_min]:
+            vertices[m+1].remove(vertices[m][j])
+            path = path + [(mi, mj)]
+            i = j
+            j = i + 1
+        else:
+            e.add(((mj, mi)))
+            l = mi + 1
+            li_min = min(l, mi)
+            li_max = max(l, mi)
+            lj_min = min(l, mj)
+            lj_max = max(l, mj)
+            print(m,j,i, l, mi, mj)
+            print(edges)
+            print(all_vertices)
+            print(vertices[m])
+            while all_vertices[l] != vertices[m][j] and (not edges[li_max][li_min] or not edges[lj_max][lj_min]):
+                li_min = min(l, mi)
+                li_max = max(l, mi)
+                lj_min = min(l, mj)
+                lj_max = max(l, mj)
+                print(m,j,i, l, mi, mj)
+                if not edges[li_max][li_min]:
+                    e.add((l, mi))
+                if not edges[lj_max][lj_min]:
+                    e.add((mj, l))
+                l = l + 1
+            if all_vertices[l] == vertices[m][j]:
+                vertices[m+1].remove(vertices[m][j])
+                path = path + [(mi, l), (l, mj)]
+                i = j
+                j = i + 1
+            else:
+                j = j + 1
+    return path
 
 def _get_index(vertex, all_vertices):
-	return all_vertices.index(vertex)
+    return all_vertices.index(vertex)
--- Test.py
+++ Test.py
@@ -3,12 +3,12 @@ from Generate import Graph
 from christofides import christofides_alg
 from BFI import find_shortest_path, bfi
 
-test_graph = Graph(20, 100)
+test_graph = Graph(5,6)
 print(test_graph)
 print(christofides_alg(test_graph))
 # print("-------")
 # print(test_graph)
+print("-------")
+path = CCTP.cyclic_routing(test_graph)
 # print("-------")
-# path = CCTP.cyclic_routing(test_graph)
-# print("-------")
-# print(path)
+print(path)
