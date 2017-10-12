3779c37baa6c8cb17edfa4d4f056a675f2ff50a9
Garrett Suss <garrettsuss@gmail.com>
2017-10-12 01:00:48 -0500

    Removed print statements

--- christofides_alg.py
+++ christofides_alg.py
@@ -7,15 +7,11 @@ def christofides_alg(graph):
     vertices = graph.vertices
     size = len(vertices)
     distance_matrix = [[0 for a in range(size)] for b in range(size)]
-    print(vertices)
-    print(distance_matrix)
     for x in range(size):
         for y in range(x+1, size):
             distance = graph.distance(vertices[x], vertices[y])
-            print(distance)
             distance_matrix[x][y] = distance
             distance_matrix[y][x] = distance
-    print(distance_matrix)
     result = christofides.compute(distance_matrix)
     solution = result['Christofides_Solution']
     path = []
